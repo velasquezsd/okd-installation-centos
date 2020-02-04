34427ec5d9b7663b0ba007701e841aff184168e7
Daniel Velasquez Usuga <dvelasquez@ibm.com>
2020-02-04 14:42:59 -0500

    Add domain .srv

--- provisioning/settings.sh
+++ provisioning/settings.sh
@@ -7,25 +7,25 @@ export OKD_VERSION=3.11
 
 #OKD Master Node Configuration
 export OKD_MASTER_IP=192.1.2.10
-export OKD_MASTER_HOSTNAME=okd-master-node
+export OKD_MASTER_HOSTNAME=okd-master-node.srv
 
 #OKD Worker Node 1 Configuration
 export OKD_WORKER_NODE_1_IP=192.1.2.11
-export OKD_WORKER_NODE_1_HOSTNAME=okd-worker-node-1
+export OKD_WORKER_NODE_1_HOSTNAME=okd-worker-node-1.srv
 
 #OKD Worker Node 2 Configuration
 export OKD_WORKER_NODE_2_IP=192.1.2.12
-export OKD_WORKER_NODE_2_HOSTNAME=okd-worker-node-2
+export OKD_WORKER_NODE_2_HOSTNAME=okd-worker-node-2.srv
 
 #OKD Infra Node 1 Configuration
 export OKD_INFRA_NODE_1_IP=192.1.2.13
-export OKD_INFRA_NODE_1_HOSTNAME=okd-infra-node-1
+export OKD_INFRA_NODE_1_HOSTNAME=okd-infra-node-1.srv
 
 #The  below setting will be used to access OKD console https://console.$DOMAIN:$API_PORT"
 #By default we can login using the URL https://console.okd.nip.io:8443
 #To access URL from your local system we need to configure master host in C:\Windows\System32\drivers\etc\hosts file as below
 #192.1.2.10  console.okd.nip.io
-export DOMAIN=okd.nip.io
+export DOMAIN=srv
 export API_PORT=8443
 
 #OKD Login Credentials
